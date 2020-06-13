03268644dade62fbb63be2749256a9b620e17746
Vanesa Mihailova <63542411+vmihailova@users.noreply.github.com>
2020-06-13 23:59:14 +0200

    Update SILab2Test.java

--- SI_Lab2_151133/src/test/java/SILab2Test.java
+++ SI_Lab2_151133/src/test/java/SILab2Test.java
@@ -26,5 +26,18 @@ class SILab2Test {
         assertEquals(false, siLab2.function(new User("pit", "pot", "john.smith@gmailcom"), users));
         assertEquals(true, siLab2.function(new User("pit", "pot", "john.smith@gmail.com"), users));
     }
+    
+    @Test
+    void everyPath(){
+        List<String> users = new ArrayList<>();
+        users.add("marthajones57");
+        users.add("CutieMary624");
+
+        assertEquals(new RuntimeException("The user argument is not initialized!"), siLab2.function(new User(null, "john", "johny@gmail.com"), users));
+        assertEquals(new RuntimeException("User already exists!"), siLab2.function(new User("john", "john", "johny@gmail.com"), users));
+        assertEquals(false, siLab2.function(new User("pit", "pot", "john.smith@gmailcom"), users));
+        assertEquals(true, siLab2.function(new User("pit", "pot", "@."), users));
+        assertEquals(true, siLab2.function(new User("pit", "pot", "john.smith@gmail.com"), users));
+    }
 
 }
